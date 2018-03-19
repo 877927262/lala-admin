ee370e880cda9fd90474c7756943ea3972ccd45e
877927262 <877927262@qq.com>
2018-03-19 12:56:30 +0800

    新增页面和编辑页面样式写好了

--- src/views/doctor/doctor.vue
+++ src/views/doctor/doctor.vue
@@ -1,107 +1,123 @@
 <template>
-	<section>
-		<!--工具条-->
-		<el-col :span="24" class="toolbar" style="padding-bottom: 0px;">
-			<el-form :inline="true" :model="filters">
-				<el-form-item>
-					<el-input v-model="filters.name" placeholder="姓名"></el-input>
-				</el-form-item>
-				<el-form-item>
-					<el-button type="primary" v-on:click="getUsers">查询</el-button>
-				</el-form-item>
-				<el-form-item>
-					<el-button type="primary" @click="handleAdd">新增</el-button>
-				</el-form-item>
-			</el-form>
-		</el-col>
+    <section>
+        <!--工具条-->
+        <el-col :span="24" class="toolbar" style="padding-bottom: 0px;">
+            <el-form :inline="true" :model="filters">
+                <el-form-item>
+                    <el-input v-model="filters.name" placeholder="姓名"></el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" v-on:click="getUsers">查询</el-button>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" @click="handleAdd">新增</el-button>
+                </el-form-item>
+            </el-form>
+        </el-col>
 
-		<!--列表-->
-		<el-table :data="users" highlight-current-row v-loading="listLoading" @selection-change="selsChange" style="width: 100%;">
-			<!-- <el-table-column type="selection" width="55">
-			</el-table-column> -->
-			<el-table-column type="index" width="60">
-			</el-table-column>
-			<el-table-column prop="name" label="姓名" width="120" sortable>
-			</el-table-column>
-			<el-table-column prop="gender" label="性别" width="100"  sortable>
-			</el-table-column>
-			<el-table-column prop="age" label="年龄" width="100" sortable>
-			</el-table-column>
-			<el-table-column prop="card_id" label="身份证号" width="150" sortable>
-			</el-table-column>
-			<el-table-column min-width="180" >
-			</el-table-column>
-			<el-table-column label="操作" width="150">
-				<template slot-scope="scope">
-					<el-button size="small" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-					<el-button type="danger" size="small" @click="handleDel(scope.$index, scope.row)">删除</el-button>
-				</template>
-			</el-table-column>
-		</el-table>
+        <!--列表-->
+        <el-table :data="users" highlight-current-row v-loading="listLoading" @selection-change="selsChange"
+                  style="width: 100%;">
+            <!-- <el-table-column type="selection" width="55">
+            </el-table-column> -->
+            <el-table-column type="index" width="60">
+            </el-table-column>
+            <el-table-column prop="name" label="姓名" width="120" sortable>
+            </el-table-column>
+            <el-table-column prop="gender" label="性别" width="100" sortable>
+            </el-table-column>
+            <el-table-column prop="age" label="年龄" width="100" sortable>
+            </el-table-column>
+            <el-table-column prop="department" label="科室" width="150" sortable>
+            </el-table-column>
+            <el-table-column min-width="180">
+            </el-table-column>
+            <el-table-column label="操作" width="150">
+                <template slot-scope="scope">
+                    <el-button size="small" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+                    <el-button type="danger" size="small" @click="handleDel(scope.$index, scope.row)">删除</el-button>
+                </template>
+            </el-table-column>
+        </el-table>
 
-		<!--工具条-->
-		<el-col :span="24" class="toolbar">
-			<!-- <el-button type="danger" @click="batchRemove" :disabled="this.sels.length===0">批量删除</el-button> -->
-			<el-pagination layout="prev, pager, next" @current-change="handleCurrentChange" :page-size="10" :total="total" style="float:right;">
-			</el-pagination>
-		</el-col>
+        <!--工具条-->
+        <el-col :span="24" class="toolbar">
+            <!-- <el-button type="danger" @click="batchRemove" :disabled="this.sels.length===0">批量删除</el-button> -->
+            <el-pagination layout="prev, pager, next" @current-change="handleCurrentChange" :page-size="10"
+                           :total="total" style="float:right;">
+            </el-pagination>
+        </el-col>
 
-		<!--编辑界面-->
-		<el-dialog title="编辑" v-model="editFormVisible" :close-on-click-modal="false">
-			<el-form :model="editForm" label-width="80px" :rules="editFormRules" ref="editForm">
-				<el-form-item label="姓名" prop="name">
-					<el-input v-model="editForm.name" auto-complete="off"></el-input>
-				</el-form-item>
-				<el-form-item label="性别">
-					<el-radio-group v-model="editForm.gender">
-						<el-radio class="radio" label="男">男</el-radio>
-						<el-radio class="radio" label="女">女</el-radio>
-					</el-radio-group>
-				</el-form-item>
-				<el-form-item label="年龄">
-					<el-input-number v-model="editForm.age" :min="0" :max="200"></el-input-number>
-				</el-form-item>
-				<el-form-item label="身份证号" prop="card_id">
-					<el-input  placeholder="输入身份证号" v-model="editForm.card_id"></el-input>
-				</el-form-item>
-				<!-- <el-form-item label="地址">
-					<el-input type="textarea" v-model="editForm.addr"></el-input>
-				</el-form-item> -->
-			</el-form>
-			<div slot="footer" class="dialog-footer">
-				<el-button @click.native="editFormVisible = false">取消</el-button>
-				<el-button type="primary" @click.native="editSubmit" :loading="editLoading">提交</el-button>
-			</div>
-		</el-dialog>
+        <!--编辑界面-->
+        <el-dialog title="编辑" v-model="editFormVisible" :close-on-click-modal="false">
+            <el-form :model="editForm" label-width="80px" :rules="editFormRules" ref="editForm">
+                <el-form-item label="姓名" prop="name">
+                    <el-input v-model="editForm.name" auto-complete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="性别">
+                    <el-radio-group v-model="editForm.gender">
+                        <el-radio class="radio" label="男">男</el-radio>
+                        <el-radio class="radio" label="女">女</el-radio>
+                    </el-radio-group>
+                </el-form-item>
+                <el-form-item label="年龄">
+                    <el-input-number v-model="editForm.age" :min="0" :max="200"></el-input-number>
+                </el-form-item>
+                <el-form-item label="科室">
+                    <el-select v-model="editForm.initDepartmentValue" placeholder="请选择">
+                        <el-option
+                                v-for="item in editForm.department"
+                                :key="item.id"
+                                :label="item.name"
+                                :value="item.id">
+                        </el-option>
+                    </el-select>
+                </el-form-item>
+                <!-- <el-form-item label="地址">
+                    <el-input type="textarea" v-model="editForm.addr"></el-input>
+                </el-form-item> -->
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click.native="editFormVisible = false">取消</el-button>
+                <el-button type="primary" @click.native="editSubmit" :loading="editLoading">提交</el-button>
+            </div>
+        </el-dialog>
 
-		<!--新增界面-->
-		<el-dialog title="新增" v-model="addFormVisible" :close-on-click-modal="false">
-			<el-form :model="addForm" label-width="80px" :rules="addFormRules" ref="addForm">
-				<el-form-item label="姓名" prop="name">
-					<el-input v-model="addForm.name" auto-complete="off"></el-input>
-				</el-form-item>
-				<el-form-item label="性别">
-					<el-radio-group v-model="addForm.gender" prop="gender">
-						<el-radio class="radio" label="男">男</el-radio>
-						<el-radio class="radio" label="女">女</el-radio>
-					</el-radio-group>
-				</el-form-item>
-				<el-form-item label="年龄" prop="age">
-					<el-input-number v-model="addForm.age" :min="0" :max="200"></el-input-number>
-				</el-form-item>
-				<el-form-item label="身份证号" prop="card_id">
-					<el-input  placeholder="输入身份证号" v-model="addForm.card_id"></el-input>
-				</el-form-item>
-				<!-- <el-form-item label="地址">
-					<el-input type="textarea" v-model="addForm.addr"></el-input>
-				</el-form-item> -->
-			</el-form>
-			<div slot="footer" class="dialog-footer">
-				<el-button @click.native="addFormVisible = false">取消</el-button>
-				<el-button type="primary" @click.native="addSubmit" :loading="addLoading">提交</el-button>
-			</div>
-		</el-dialog>
-	</section>
+        <!--新增界面-->
+        <el-dialog title="新增" v-model="addFormVisible" :close-on-click-modal="false">
+            <el-form :model="addForm" label-width="80px" :rules="addFormRules" ref="addForm">
+                <el-form-item label="姓名" prop="name">
+                    <el-input v-model="addForm.name" auto-complete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="性别">
+                    <el-radio-group v-model="addForm.gender" prop="gender">
+                        <el-radio class="radio" label="男">男</el-radio>
+                        <el-radio class="radio" label="女">女</el-radio>
+                    </el-radio-group>
+                </el-form-item>
+                <el-form-item label="年龄" prop="age">
+                    <el-input-number v-model="addForm.age" :min="0" :max="200"></el-input-number>
+                </el-form-item>
+                <el-form-item label="科室">
+                    <el-select v-model="addForm.initDepartmentValue" placeholder="请选择">
+                        <el-option
+                                v-for="item in addForm.department"
+                                :key="item.id"
+                                :label="item.name"
+                                :value="item.id">
+                        </el-option>
+                    </el-select>
+                </el-form-item>
+                <!-- <el-form-item label="地址">
+                    <el-input type="textarea" v-model="addForm.addr"></el-input>
+                </el-form-item> -->
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click.native="addFormVisible = false">取消</el-button>
+                <el-button type="primary" @click.native="addSubmit" :loading="addLoading">提交</el-button>
+            </div>
+        </el-dialog>
+    </section>
 </template>
 
 <script>
@@ -109,7 +125,7 @@
     import axios from 'axios';
 
     // import NProgress from 'nprogress'
-    import { getUserListPage, deleteUser, editUser, addUser } from '../../api/api';
+    import {getUserListPage, deleteUser, editUser, addUser} from '../../api/api';
 
     export default {
         data() {
@@ -127,10 +143,10 @@
                 editLoading: false,
                 editFormRules: {
                     name: [
-                        { required: true, message: '请输入姓名', trigger: 'blur' }
+                        {required: true, message: '请输入姓名', trigger: 'blur'}
                     ],
                     card_id: [
-                        { required: true, message: '请输入身份证号', trigger: 'blur' }
+                        {required: true, message: '请输入身份证号', trigger: 'blur'}
                     ]
                 },
                 //编辑界面数据
@@ -139,17 +155,28 @@
                     name: '',
                     gender: '',
                     age: 0,
-                    card_id: ''
+                    card_id: '',
+                    department: [{
+                        name: '呵呵科',
+                        img: '11',
+                        id: '1'
+                    }, {
+                        name: '哈哈科',
+                        img: '22',
+                        id: '2'
+                    }],
+                    //下拉菜单的默认值，如果设置为空，就显示请选择
+                    initDepartmentValue: ''
                 },
 
                 addFormVisible: false,//新增界面是否显示
                 addLoading: false,
                 addFormRules: {
                     name: [
-                        { required: true, message: '请输入姓名', trigger: 'blur' }
+                        {required: true, message: '请输入姓名', trigger: 'blur'}
                     ],
                     card_id: [
-                        { required: true, message: '请输入身份证号', trigger: 'blur' }
+                        {required: true, message: '请输入身份证号', trigger: 'blur'}
                     ]
                 },
                 //新增界面数据
@@ -157,9 +184,20 @@
                     name: '',
                     gender: '',
                     age: 0,
-                    card_id: ''
-                }
-
+                    card_id: '',
+                    //科室下拉菜单数据
+                    department: [{
+                        name: '呵呵科',
+                        img: '11',
+                        id: '1'
+                    }, {
+                        name: '哈哈科',
+                        img: '22',
+                        id: '2'
+                    }],
+                    //下拉菜单的默认值，如果设置为空，就显示请选择
+                    initDepartmentValue: ''
+                },
             }
         },
         methods: {
@@ -204,17 +242,16 @@
             //显示编辑界面
             handleEdit: function (index, row) {
                 this.editFormVisible = true;
+                //在这里获取一下科室
                 this.editForm = Object.assign({}, row);
             },
-            //显示新增界面
+            //显示新增界面，并且将数据清空
             handleAdd: function () {
                 this.addFormVisible = true;
-                this.addForm = {
-                    name: '',
-                    gender: '男',
-                    age: 0,
-                    card_id: ''
-                };
+                this.addForm.name = '';
+                this.addForm.gender = '男';
+                this.addForm.age = 0;
+                this.addForm.card_id = '';
             },
             //编辑
             editSubmit: function () {
@@ -273,7 +310,7 @@
                 }).then(() => {
                     this.listLoading = true;
                     //NProgress.start();
-                    let para = { ids: ids };
+                    let para = {ids: ids};
                     batchRemoveUser(para).then((res) => {
                         this.listLoading = false;
                         //NProgress.done();
