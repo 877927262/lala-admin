36b39fb84d9b8dda4d295a5215269327f652f44c
877927262 <877927262@qq.com>
2018-03-19 10:16:20 +0800

    开题报告写完了，加了几行注释，记下来接着写项目

--- src/api/api.js
+++ src/api/api.js
@@ -5,19 +5,19 @@ let base = 'http://127.0.0.1:3000/appointment';
 export const requestLogin = params => {
   return axios.post(`/login`, params).then(res => res.data);
 };
-
+// 查询用户
 export const getUserListPage = (name, page) => {
   return axios.get(`${base}/getuser`, { params:{name: name, page: page }});
  };
-
+// 删除用户
 export const deleteUser = params => {
   return axios.get(`${base}/deleteUser`, { params: params });
 };
-
+// 编辑用户
 export const editUser = params => {
   return axios.post(`${base}/editUser`, params)
 };
-
+// 新增用户
 export const addUser = params => {
   return axios.post(`${base}/addUser`, {
     name: params.name,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -37,7 +37,7 @@
 					<li v-for="(item,index) in $router.options.routes" v-if="!item.hidden" class="el-submenu item">
 						<template v-if="!item.leaf">
 							<div class="el-submenu__title" style="padding-left: 20px;" @mouseover="showMenu(index,true)" @mouseout="showMenu(index,false)"><i :class="item.iconCls"></i></div>
-							<ul class="el-menu submenu" :class="'submenu-hook-'+index" @mouseover="showMenu(index,true)" @mouseout="showMenu(index,false)"> 
+							<ul class="el-menu submenu" :class="'submenu-hook-'+index" @mouseover="showMenu(index,true)" @mouseout="showMenu(index,false)">
 								<li v-for="child in item.children" v-if="!child.hidden" :key="child.path" class="el-menu-item" style="padding-left: 40px;" :class="$route.path==child.path?'is-active':''" @click="$router.push(child.path)">{{child.name}}</li>
 							</ul>
 						</template>
@@ -74,7 +74,7 @@
 	export default {
 		data() {
 			return {
-				sysName:'医院挂号平台',
+				sysName:'社区医院挂号平台',
 				collapsed:false,
 				sysUserName: '',
 				sysUserAvatar: '',
@@ -139,7 +139,7 @@
 
 <style scoped lang="scss">
 	@import '~scss_vars';
-	
+
 	.container {
 		position: absolute;
 		top: 0px;
@@ -235,7 +235,7 @@
 				width: 60px;
 			}
 			.menu-expanded{
-				
+
 				flex: 0 0 180px;
 				width: 180px;
 				.el-menu {
